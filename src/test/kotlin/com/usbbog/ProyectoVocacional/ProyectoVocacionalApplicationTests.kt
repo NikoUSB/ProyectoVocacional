48ae9a0ab4ce944d1bcf7d60dead22fe10a940bc
NICOLAS ALVARADO SORIANO <nalvarados@academia.usbbog.edu.co>
2026-04-09 07:57:38 -0500

    Initial Commit
    Subir el proyecto base

---
+++ src/test/kotlin/com/usbbog/ProyectoVocacional/ProyectoVocacionalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.usbbog.ProyectoVocacional
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyectoVocacionalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
